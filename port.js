0c2ea67f6c55d03a84799155bdf6cbaf7332d532
unbalanced <unbalanced@unbalanceds-MacBook-Pro.local>
2015-06-22 01:59:58 -0300

    removed console.log

--- port.js
+++ port.js
@@ -4,14 +4,11 @@ var radio = Elm.fullscreen(Elm.Radio);
 
 radio.ports.name.subscribe(function (state){
 
-    console.log(state);
-
     if (audio5js && typeof audio5js.destroy == 'function') {
         audio5js.destroy();
     }
 
     if (state.playing === true) {
-
         audio5js = new Audio5js({
             swf_path: './audio5js.swf',
             ready: function () {
